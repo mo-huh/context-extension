4aa21e13ab7971579be57910c57ca6831f38258a
mo-huh <50864651+mo-huh@users.noreply.github.com>
2025-01-03 15:38:23 +0100

    Delete hello_world.py

--- hello_world.py
+++
@@ -1,6 +0,0 @@
-import torch
-print(torch.cuda.is_available())  # Gibt True aus, wenn GPU verfügbar
-print(torch.cuda.device_count())  # Gibt die Anzahl der GPUs zurück
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
